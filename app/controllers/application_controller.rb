288df7f75eabb6416878b4a880a185f270033e56
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 13:40:38 +0100

    authorized home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
 
   private
   
