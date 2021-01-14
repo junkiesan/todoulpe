bc5952513eb1c528f5d5d4d65de2ea1eaa8c0b97
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 19:19:54 +0100

    send error if deadline is over

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,12 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { in: 3..30,
     too_long: "30 characters is the maximum allowed"}
+  validates :check_expiracy
 
+  # Send error if deadline expired
+  def check_expiracy
+    errors.add(:deadline, "the deadline is here") if self.deadline.expired?
+  end
   # Checking if deadline is not expired
   def expired?
     self.deadline > Date.today ? priority == "hardcore" : priority
