85c76f5960e7345be696fac524a9f5f1df5f0ce6
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 16:06:56 +0100

    Tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/update'
+  get 'tasks/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
