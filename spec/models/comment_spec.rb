c91d7473d64da9f6d7dd394721879d0e564a244c
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 18:34:14 +0100

    Rspec comment CRUD

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  @user = User.create(email: 'poulpo@todoulpe.com', password: '123456')
-  @task = Task.create!(title: "Coding a todo app", priority: "hardcore", deadline: Time.now, status: false, user_id: User.last.id)
+  # @user = User.new
+  # @task = Task.create!(title: "Coding a todo app", priority: "hardcore", deadline: Time.now, status: false, user_id: User.last.id)
   @comment = Comment.create(details: "I should add more tentacles")
+end
+
+ # CRUD test
+describe Comment, "#create" do
+  it 'checks that a comment can be created' do
+    comment = Comment.create(details: "Such a good idea!", task_id: Task.last.id)
+    expect(comment).to be_valid
+  end
+end
+
+describe Comment, "#show" do
+  it 'checks that a comment can be read' do
+    comment = Comment.create(details: "I should add more tentacles", task_id: Task.last.id)
+    expect(Comment.find_by_details("I should add more tentacles")).to eq(comment)
+  end
+end
+
+describe Comment, "#update" do
+  it 'checks that a comment can be updated' do
+    comment = Comment.create(details: "I should add more tentacles", task_id: Task.last.id)
+    comment.update(:details => "I should add more tentacles and more colors")
+    expect(Comment.find_by_details("I should add more tentacles and more colors")).to eq(comment)
+  end
+end
+
+describe Comment, "#destroy" do
+  it 'checks that a comment can be destroyed' do
+    comment = Comment.create(details: "I should add more tentacles and more colors", task_id: Task.last.id)
+    comment.destroy
+    expect(Comment.find_by(details: "I should add more tentacles and more colors")).to be_nil
+  end
 end
