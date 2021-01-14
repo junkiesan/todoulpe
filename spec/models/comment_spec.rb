1384fcde7ba70c3159c868b07c481cdf12d64d80
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 17:46:54 +0100

    Comment type

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  @user = User.create(email: 'poulpo@todoulpe.com', password: '123456')
+  @task = Task.create!(title: "Coding a todo app", priority: "hardcore", deadline: Time.now, status: false, user_id: User.last.id)
+  @comment = Comment.create(details: "I should add more tentacles")
 end
