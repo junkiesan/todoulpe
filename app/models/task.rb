34a2872d35486e8480c114ecdaf1576f6bbf3c9c
Julien Bouland <bouland.julien@gmail.com>
2021-01-14 19:16:29 +0100

    validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
   # Associations 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  # Priority constant
-  PRIORITY = ["chill", "work", "hardcore"]
+  # Validations
+  validates :priority, inclusion: { in: ["chill", "work", "hardcore"] }
+  validates :title, presence: true
+  validates :title, length: { in: 3..30,
+    too_long: "30 characters is the maximum allowed"}
+
+  # Checking if deadline is not expired
+  def expired?
+    self.deadline > Date.today ? priority == "hardcore" : priority
+  end
 end
