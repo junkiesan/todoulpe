57170644b56979667c88fd89f49302c400df0a36
Julien Bouland <bouland.julien@gmail.com>
2021-01-15 13:47:15 +0100

    bug fix: comment creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -66,6 +66,6 @@ class TasksController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def task_params
-    params.require(:task).permit(:title, :priority, :deadline, :status)
+    params.permit(:title, :priority, :deadline, :status)
   end
 end
