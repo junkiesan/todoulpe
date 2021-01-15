6ee13a3fe456d5cd1e3ea117396b5ffb9217eec6
Julien Bouland <bouland.julien@gmail.com>
2021-01-15 12:43:10 +0100

    private set callback for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
   def index
   end
 
@@ -19,6 +21,11 @@ class CommentsController < ApplicationController
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+  # Only allow a trusted parameter "white list" through.
   def comment_params
     params.require(:comment).permit(:details)
   end
