99f79e4ae888282e9f4bf9c173773be2d59e0666
Julien Bouland <bouland.julien@gmail.com>
2021-01-15 14:01:05 +0100

    Task Policy Generated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  include Pundit
   before_action :authenticate_user!
+  include Pundit
   after_action :verify_authorized, except: :index, unless: :devise_controller?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
 
   private
