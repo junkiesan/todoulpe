c1e9b2cc598b64fc0f5c120caa193e7613a146dd
Julien Bouland <bouland.julien@gmail.com>
2021-01-15 14:07:37 +0100

    Task Policy Done

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,7 +1,34 @@
 class TaskPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
+    # Legend
+    # - record: the truck passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  def index
+    @tasks = policy_scope(Task)
+    record.user == current_user
+  end
+  
+    def show?
+      record.user == current_user
+    end
+  
+    def create?
+      record.user == current_user
+    end
+  
+    def update?
+      record.user == current_user
+    end
+  
+    def destroy?
+      record.user == current_user
+    end
+  
+    def write_comment
+      true # Anyone can write a comment
+    end
 end
