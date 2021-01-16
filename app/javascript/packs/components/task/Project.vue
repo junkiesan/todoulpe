825cbaa1f430544055800ffa61ace19823dd9d67
Julien Bouland <bouland.julien@gmail.com>
2021-01-16 16:06:58 +0100

    new task form

--- app/javascript/packs/components/task/Project.vue
+++ app/javascript/packs/components/task/Project.vue
@@ -1,5 +1,25 @@
 <template>
-  <p>Hello !!!!!</p>
+  <div class="container">
+    <!-- NEW TASK FORM -->
+    <div class="row new">
+      <form class="form-inline">
+        <button v-on:click="createTask">
+          <i class="material-icons">add</i>
+        </button>
+        <label class="sr-only" for="inline-email">Titre</label>
+        <input type="text" class="form-control mb-2 mr-sm-2" id="inline-email new-task-form" placeholder="Votre tâche" v-model="newTask" required>
+
+        <label class="sr-only" for="inline-password">Priorité</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>chill</option>
+          <option>work</option>
+          <option>harcore</option>
+        </select>
+
+        <label class="sr-only" for="inline-password">Date limite</label>
+        <input type="date" class="form-control mb-2 mr-sm-2" id="inline-password">
+      </form>
+  </div>
 </template>
 
 <script>
@@ -16,5 +36,9 @@ export default {
 </script>
 
 <style>
-
+  .new {
+    display: flex;
+    flex-direction: row;
+    align-items: center
+  }
 </style>
