ed63d02de58053a348c4785a460f135aa661b8b4
Julien Bouland <bouland.julien@gmail.com>
2021-01-16 23:37:17 +0100

    defacto for project

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,49 @@
 <template>
   <div id="app">
     <navheader></navheader>
-    <tabProject></tabProject>
-    <div>
-      <ul class="container">
-        <li v-if="task.status === false" v-for="task in tasks" :key="task.id">
-          <input type="checkbox"  v-on:change="doneTask(task.id)" v-model="task.status" />
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.title }}</label>
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.priority }}</label>
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.deadline }}</label>
-        </li>
-      </ul>
-      <ul class="container">
-        <li v-if="task.status === true" v-for="task in tasks" :key="task.id">
-          <input type="checkbox" />
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.title }}</label>
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.priority }}</label>
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.deadline }}</label>
-        </li>
-      </ul>
+       <!-- NEW TASK FORM -->
+    <div class="container border">
+      <div class="row new">
+        <form class="form-inline" @submit.prevent="addTask">
+          <button v-on:click="addTask">
+            <i class="material-icons">add</i>
+          </button>
+          <label class="sr-only" for="inline-email">Titre</label>
+          <input type="text" class="form-control mb-2 mr-sm-2" id="inline-email new-task-form" placeholder="Votre tâche" v-model="newTask" required>
+
+          <label class="sr-only" for="inline-password">Priorité</label>
+          <select class="form-control" id="exampleFormControlSelect1" v-model="newTaskPriority" >
+            <option>chill</option>
+            <option>work</option>
+            <option>harcore</option>
+          </select>
+
+          <label class="sr-only" for="inline-password">Date limite</label>
+          <input type="date" class="form-control mb-2 mr-sm-2" id="inline-password" v-model="newTaskDeadline">
+        </form>
+      </div>
+
+  <!-- DISPLAY TASK NOT DONE  -->
+      <div>
+        <ul class="container">
+          <li v-if="task.status === false" v-for="task in tasks" :key="task.id">
+            <input type="checkbox"  v-on:change="doneTask(task.id)" v-model="task.status" />
+            <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.title }}</label>
+            <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.priority }}</label>
+            <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.deadline }}</label>
+          </li>
+        </ul>
+        
+  <!-- DISPLAY TASK  DONE  -->
+        <ul class="container">
+          <li v-if="task.status === true" v-for="task in tasks" :key="task.id">
+              <input type="checkbox" />
+              <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.title }}</label>
+              <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.priority }}</label>
+              <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.deadline }}</label> 
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -26,18 +51,17 @@
 <script>
 import Rails from '@rails/ujs';
 import Header from './packs/components/Header.vue'
-import Project from './packs/components/task/Project.vue'
 
 export default {
   components: {
     navheader: Header,
-    tabProject: Project
   },
   props: ['original_tasks'],
   data: function () {
     return {
       tasks: this.original_tasks,
       status: false,
+      newTask: '',
     }
   },
   methods: {
@@ -58,6 +82,27 @@ export default {
         }
       })
     },
+    addTask: function () {
+      var data = new FormData
+      data.append("task[title]", this.newTask)
+      data.append("task[deadline]", this.newTaskDeadline)
+      data.append("task[priority]", this.newTaskPriority)
+      
+      Rails.ajax({
+          url: "/tasks",
+          type: "POST",
+          data: data,
+          dataType: "json",
+          success: (data) => {
+            const index = window.store.tasks.findIndex(item => item.id == this.list.id)
+            window.store.tasks[index].tasks.push(data)
+            this.message = ""
+            this.$nextTick(() => { this.$refs.newTask.focus() })
+            this.$nextTick(() => { this.$refs.newTaskDeadline.focus() })
+            this.$nextTick(() => { this.$refs.newTaskPriority.focus() })
+          }
+      })
+    },
   }
 }
 </script>
@@ -74,5 +119,16 @@ export default {
     font-family: 'Space Mono', monospace;
     font-size: 2em;
     text-align: center;
+  }
+
+  .new {
+    display: flex;
+    flex-direction: row;
+    align-items: center
+  }
+
+  .border {
+    border: 10px solid #BCEED9;
+    border-radius: 5px;
   }
 </style>
--- app/javascript/packs/components/task/Project.vue
+++ app/javascript/packs/components/task/Project.vue
@@ -1,42 +1,5 @@
 <template>
-  <div class="container border">
-    <!-- NEW TASK FORM -->
-    <div class="row new">
-      <form class="form-inline" @submit.prevent="addTask">
-        <button v-on:click="addTask">
-          <i class="material-icons">add</i>
-        </button>
-        <label class="sr-only" for="inline-email">Titre</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="inline-email new-task-form" placeholder="Votre tâche" v-model="newTask" required>
-
-        <label class="sr-only" for="inline-password">Priorité</label>
-        <select class="form-control" id="exampleFormControlSelect1" v-model="newTaskPriority" >
-          <option>chill</option>
-          <option>work</option>
-          <option>harcore</option>
-        </select>
-
-        <label class="sr-only" for="inline-password">Date limite</label>
-        <input type="date" class="form-control mb-2 mr-sm-2" id="inline-password" v-model="newTaskDeadline">
-      </form>
-    </div>
-
-    <!-- DISPLAY TASK NOT DONE  -->
-    <div>
-      <ul class="container">
-        <li></li>
-      </ul>
-    </div>
-    <!-- DISPLAY TASK NOT DONE  -->
-    <!-- <div>
-      <ul class="container">
-        <li v-for="task in listTasks" :key="task.status">
-          <input type="checkbox" v-bind:id="'task_' + task.id" v-on:change="doneTask(task.id)" />
-          <label v-bind:for="'task_' + task.id" class="word-color-black">{{ task.name }}</label>
-        </li>
-      </ul>
-    </div> -->
-  </div>
+  
 </template>
 
 <script>
@@ -79,14 +42,5 @@ export default {
 </script>
 
 <style>
-  .new {
-    display: flex;
-    flex-direction: row;
-    align-items: center
-  }
 
-  .border {
-    border: 10px solid #BCEED9;
-    border-radius: 5px;
-  }
 </style>
