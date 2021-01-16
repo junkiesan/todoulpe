9e76f259d8804550fac6746c4444f4e1e4ec7429
Julien Bouland <bouland.julien@gmail.com>
2021-01-16 13:00:29 +0100

    vue turbolinks added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,14 +39,33 @@ require("channels")
   //   }
   // });
 
-  import Vue from 'vue'
-  import App from '../app.vue'
+  // import Vue from 'vue'
+  // import App from '../app.vue'
   
-  document.addEventListener('DOMContentLoaded', () => {
-    const app = new Vue({
-      render: h => h(App)
-    }).$mount()
-    document.body.appendChild(app.$el)
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   const app = new Vue({
+  //     render: h => h(App)
+  //   }).$mount()
+  //   document.body.appendChild(app.$el)
   
-    console.log(app)
+  //   console.log(app)
+  // })
+
+
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#hello',
+    data: () => {
+      return {
+        message: "Can you say hello?"
+      }
+    },
+    components: { App }
   })
+})
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -52,21 +52,3 @@ document.addEventListener('DOMContentLoaded', () => {
 // yarn add vue-turbolinks
 //
 // Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
